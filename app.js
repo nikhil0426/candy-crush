043586e2dec91406d45bfb72996f5a59402287be
Nikhil <nikhil0426@gmail.com>
2021-03-21 18:55:58 -0400

    randomize color assignment of candies

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 8
   const candies = []
 
+  const candyColors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
   // Make my board
   function drawBoard () {
     for (let i = 0; i < Math.pow(width, 2); i++) {
       const candy = document.createElement('div')
+      let randomizeColors = Math.floor(Math.random() * candyColors.length)
+      candy.style.background = candyColors[randomizeColors]
       arr.appendChild(candy)
       candies.push(candy)
     }
